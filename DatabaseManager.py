153f096585432d0fc99c87b3b181042832ec5ebe
skvaryk <marek.alexa@synetech.cz>
2019-01-10 13:35:15 +0100

    added login with gmail and prizes page

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -1,3 +1,4 @@
+import pymongo
 from pymongo import MongoClient
 
 
@@ -8,30 +9,48 @@ class DatabaseManager:
         self.client = MongoClient(host=database_uri, port=database_port)
         self.db = self.client[self.DATABASE_NAME]
         if use_test_data:
-            self.fill_db_with_test_data()
-
-    def fill_db_with_test_data(self):
-        self.client.drop_database(self.DATABASE_NAME)
-        self.db = self.client[self.DATABASE_NAME]
-        user1 = {'email': 'franta.pepa1@synetech.cz', 'points': 3}
-        user2 = {'email': 'franta.pepa2@synetech.cz', 'points': 4}
-        user3 = {'email': 'franta.pepa3@synetech.cz', 'points': 5}
-        self.store_user(user1)
-        self.store_user(user2)
-        self.store_user(user3)
+            self._fill_db_with_test_data()
 
     def __del__(self):
         self.client.close()
 
     def store_user(self, user):
-        result = self.db.users.insert_one(user)
-        return result
+        return self.db.users.insert_one(user)
 
-    def get_user(self, user_id):
-        query = {'_id': user_id}
+    def get_user(self, email):
+        query = {'email': email}
         result = self.db.users.find(query)
         return result
 
     def get_all_users(self):
-        result = self.db.users.find()
+        result = self.db.users.find().sort([('points', pymongo.DESCENDING)])
         return result
+
+    def store_prize(self, prize):
+        return self.db.prizes.insert_one(prize)
+
+    def get_all_prizes(self):
+        return self.db.prizes.find()
+
+    def _fill_db_with_test_data(self):
+        self._drop_db()
+
+        self.store_user({'email': 'franta.pepa1@synetech.cz', 'points': 3})
+        self.store_user({'email': 'franta.pepa3@synetech.cz', 'points': 5})
+        self.store_user({'email': 'franta.pepa2@synetech.cz', 'points': 1})
+
+        self.store_prize({'name': 'Školení', 'price': 'dle domluvy'})
+        self.store_prize({'name': 'Půjčení firemního drona na 3 dny', 'price': '50'})
+        self.store_prize({'name': 'Půjčení firemního auta na 3 dny', 'price': '65'})
+        self.store_prize({'name': 'Alza poukaz - 1000 Kč', 'price': '80'})
+        self.store_prize({'name': 'Předplatné Spotify Premium na půl roku', 'price': '120'})
+        self.store_prize({'name': 'Poukaz na gastronomický zážitek pro dva', 'price': '150'})
+        self.store_prize({'name': 'Předplatné Netflix Standard na půl roku', 'price': '150'})
+        self.store_prize({'name': 'Proplacení jízd Uberem do limitu 2000 Kč', 'price': '200'})
+        self.store_prize({'name': 'Offroadový zážitek s Vráťou a jeho princeznou Ladou', 'price': '300'})
+        self.store_prize({'name': 'Alza poukaz - 5000 Kč', 'price': '400'})
+        self.store_prize({'name': 'Půjčení IPhone X či Android ekvivalent na rok', 'price': '1000'})
+
+    def _drop_db(self):
+        self.client.drop_database(self.DATABASE_NAME)
+        self.db = self.client[self.DATABASE_NAME]
--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import json
 import os
 
-from flask import Flask, render_template, request
+import requests
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_dance.consumer import oauth_authorized
+from flask_dance.contrib.google import make_google_blueprint, google
 
 from DatabaseManager import DatabaseManager
 
@@ -10,10 +14,48 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 database_manager = DatabaseManager(app.config["MONGO_DATABASE_URI"], app.config["MONGO_DATABASE_PORT"],
                                    use_test_data=True)
 
+# don't require https - only for local testing
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+with open('client_id.json') as f:
+    client_id = json.load(f)
+    web = client_id['web']
+
+    google_bp = make_google_blueprint(
+        client_id=web['client_id'],
+        client_secret=web['client_secret'],
+        scope=["https://www.googleapis.com/auth/plus.me",
+               "https://www.googleapis.com/auth/userinfo.email", ],
+        hosted_domain="synetech.cz"
+    )
+
+    app.register_blueprint(google_bp, url_prefix="/login")
+
+
+@oauth_authorized.connect_via(google_bp)
+def logged_in(blueprint, token):
+    resp_json = google.get("/oauth2/v2/userinfo").json()
+    if resp_json["hd"] != blueprint.authorization_url_params["hd"]:
+        requests.post(
+            "https://accounts.google.com/o/oauth2/revoke",
+            params={"token": token["access_token"]}
+        )
+        session.clear()
+        os.abort(403)
+
+
+@app.route("/login/google/authorized")
+def authorized():
+    print("authorized")
+
+
+@app.route("/")
+def index():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/oauth2/v2/userinfo")
+    assert resp.ok, resp.text
+    return "You are {email} on Google".format(email=resp.json()["email"])
 
 
 @app.route('/users', methods=['GET', 'POST'])
@@ -28,7 +70,14 @@ def users():
             database_manager.store_user(user)
 
     # elif request.method == 'GET':
-    return render_template('users.html', my_string="Hello", my_list=list(database_manager.get_all_users()))
+    return render_template('users.html', my_string="Hello",
+                           my_list=list(database_manager.get_all_users()))
+
+
+@app.route('/prizes')
+def prizes():
+    return render_template('prizes.html', my_string="Hello",
+                           my_list=list(database_manager.get_all_prizes()))
 
 
 if __name__ == '__main__':
